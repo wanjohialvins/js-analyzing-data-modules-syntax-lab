9db4d4e0e29d1eecb71e9591ce2a70edf6d2a855
wanjohialvins <wanjohialvinsgmail.com>
2025-12-19 12:34:14 +0300

    created function to merge array into users and merge the date

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+require('datejs');
 
+function combineUsers(...args) {
+  // Initialize return object
+  const combinedObject = {
+    users: []
+  };
 
+  // Loop through args 
+  args.forEach(function (userArray) {
+    // Merge each array into users using spread operator
+    combinedObject.users.push(...userArray);
+  });
+
+  // Merge date in M/d/yyyy format
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
